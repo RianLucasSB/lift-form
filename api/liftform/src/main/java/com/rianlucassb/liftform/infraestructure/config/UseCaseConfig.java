1100ded42f4ad3ebf99dc9adf2caa1a297512448
rianlucas2014 <rianlucas2014@gmail.com>
2026-04-02 20:02:03 -0300

    feat: implement refresh token controller

--- api/liftform/src/main/java/com/rianlucassb/liftform/infraestructure/config/UseCaseConfig.java
+++ api/liftform/src/main/java/com/rianlucassb/liftform/infraestructure/config/UseCaseConfig.java
@@ -4,9 +4,12 @@ import com.rianlucassb.liftform.core.gateway.security.*;
 import com.rianlucassb.liftform.core.gateway.user.UserRepository;
 import com.rianlucassb.liftform.core.usecases.user.login.LoginUseCase;
 import com.rianlucassb.liftform.core.usecases.user.login.LoginUseCaseImpl;
+import com.rianlucassb.liftform.core.usecases.user.refreshtoken.RefreshTokenUseCase;
+import com.rianlucassb.liftform.core.usecases.user.refreshtoken.RefreshTokenUseCaseImpl;
 import com.rianlucassb.liftform.core.usecases.user.register.RegisterUseCase;
 import com.rianlucassb.liftform.core.usecases.user.register.RegisterUseCaseImpl;
 import com.rianlucassb.liftform.infraestructure.transaction.TransactionalLoginUseCase;
+import com.rianlucassb.liftform.infraestructure.transaction.TransactionalRefreshTokenUseCase;
 import com.rianlucassb.liftform.infraestructure.transaction.TransactionalRegisterUseCase;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
@@ -68,5 +71,30 @@ public class UseCaseConfig {
             @Qualifier("loginUseCaseImpl") LoginUseCase loginUseCase
     ) {
         return new TransactionalLoginUseCase(loginUseCase);
+    }
+
+    @Bean("refreshTokenUseCaseImpl")
+    public RefreshTokenUseCase refreshTokenUseCase(
+            RefreshTokenRepository refreshTokenRepository,
+            RefreshTokenHasher refreshTokenHasher,
+            RefreshTokenGenerator refreshTokenGenerator,
+            AccessTokenGenerator accessTokenGenerator,
+            UserRepository userRepository
+    ) {
+        return new RefreshTokenUseCaseImpl(
+                refreshTokenRepository,
+                refreshTokenHasher,
+                refreshTokenGenerator,
+                accessTokenGenerator,
+                userRepository
+        );
+    }
+
+    @Bean
+    @Primary
+    public RefreshTokenUseCase transactionalRefreshTokenUseCase(
+            @Qualifier("refreshTokenUseCaseImpl") RefreshTokenUseCase refreshTokenUseCase
+    ) {
+        return new TransactionalRefreshTokenUseCase(refreshTokenUseCase);
     }
 }
--- api/liftform/src/main/java/com/rianlucassb/liftform/presentation/controller/AuthController.java
+++ api/liftform/src/main/java/com/rianlucassb/liftform/presentation/controller/AuthController.java
@@ -1,24 +1,22 @@
 package com.rianlucassb.liftform.presentation.controller;
 
 import com.rianlucassb.liftform.core.usecases.user.login.LoginUseCase;
+import com.rianlucassb.liftform.core.usecases.user.refreshtoken.RefreshTokenUseCase;
+import com.rianlucassb.liftform.core.usecases.user.refreshtoken.RefreshTokenUseCaseInput;
 import com.rianlucassb.liftform.core.usecases.user.register.RegisterUseCase;
+import com.rianlucassb.liftform.core.usecases.user.register.RegisterUseCaseOutput;
 import com.rianlucassb.liftform.presentation.constants.ApiPaths;
-import com.rianlucassb.liftform.presentation.dto.LoginRequestDTO;
-import com.rianlucassb.liftform.presentation.dto.LoginResponseDTO;
-import com.rianlucassb.liftform.presentation.dto.RegisterRequestDTO;
-import com.rianlucassb.liftform.presentation.dto.RegisterResponseDTO;
+import com.rianlucassb.liftform.presentation.dto.*;
 import com.rianlucassb.liftform.presentation.mapper.LoginUseCaseMapper;
 import com.rianlucassb.liftform.presentation.mapper.RegisterUseCaseMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.Duration;
 
@@ -28,6 +26,7 @@ import java.time.Duration;
 public class AuthController {
     private final LoginUseCase loginUseCase;
     private final LoginUseCaseMapper loginUseCaseMapper;
+    private final RefreshTokenUseCase refreshTokenUseCase;
 
     private final RegisterUseCase registerUseCase;
     private final RegisterUseCaseMapper registerUseCaseMapper;
@@ -40,13 +39,7 @@ public class AuthController {
         var input = loginUseCaseMapper.toUseCaseInput(request);
         var output = loginUseCase.execute(input);
 
-        ResponseCookie refreshCookie = ResponseCookie.from("refresh_token", output.refreshToken())
-                .httpOnly(true)
-                .secure(cookieSecure)
-                .path("/api/v1/auth/refresh")
-                .maxAge(Duration.ofDays(10))
-                .sameSite("Strict")
-                .build();
+        ResponseCookie refreshCookie = getRefreshCookie(output.refreshToken());
 
         return ResponseEntity.ok()
                 .header(HttpHeaders.SET_COOKIE, refreshCookie.toString())
@@ -57,16 +50,33 @@ public class AuthController {
     public ResponseEntity<RegisterResponseDTO> register(@RequestBody @Valid RegisterRequestDTO request) {
         var input = registerUseCaseMapper.toUseCaseInput(request);
         var output = registerUseCase.execute(input);
-        ResponseCookie refreshCookie = ResponseCookie.from("refresh_token", output.refreshToken())
+
+        ResponseCookie refreshCookie = getRefreshCookie(output.refreshToken());
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, refreshCookie.toString())
+                .body(registerUseCaseMapper.toResponseDTO(output));
+    }
+
+    @PostMapping("/refresh")
+    public ResponseEntity<RefreshTokenResponseDTO> refreshToken(@CookieValue (name = "refresh_token", required = false) String refreshToken) {
+        var input = new RefreshTokenUseCaseInput(refreshToken);
+        var output = refreshTokenUseCase.execute(input);
+
+        ResponseCookie refreshCookie = getRefreshCookie(output.refreshToken());
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.SET_COOKIE, refreshCookie.toString())
+                .body(new RefreshTokenResponseDTO(output.accessToken()));
+    }
+
+    private @NonNull ResponseCookie getRefreshCookie(String output) {
+        return ResponseCookie.from("refresh_token", output)
                 .httpOnly(true)
                 .secure(cookieSecure)
                 .path("/api/v1/auth/refresh")
                 .maxAge(Duration.ofDays(10))
                 .sameSite("Strict")
                 .build();
-
-        return ResponseEntity.ok()
-                .header(HttpHeaders.SET_COOKIE, refreshCookie.toString())
-                .body(registerUseCaseMapper.toResponseDTO(output));
     }
 }
